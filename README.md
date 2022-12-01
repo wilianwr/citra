4b45479879856e77050d1fa259b0f746e1849026
Wilian Robal Dos Santos <wiliamrobaldossantos1@gmail.com>
2022-11-30 21:13:08 -0300

    Adicionado vosk/pyaudio

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # citra
  Assitente Virtual em Python
+
+Foco: 
+   Facil de aprender.
+   Rápido e expansível.
+
+Tecnologias:
+   Reconhecimento de voz: (Google, )
+   Sintese de voz.
+   Algum de tipo IA.
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # Our main file.
+
+import speech_recognition as sr
+
+# Cria um reconhecedor
+r = sr.Recognizer()
+
+# Abir o microfone para captura
+with sr.Microphone() as source:
+
+ while True:
+    audio = r.listen(source) # Define o microfone como fonte de áudio
+
+    print(r.recognize_google(audio, language='pt'))
